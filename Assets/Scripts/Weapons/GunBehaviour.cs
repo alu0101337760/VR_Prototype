6a0d78fba71b061f1bedc71ef59b825f2720a535
Enrique Viña Alonso <alu0101337760@ull.edu.es>
2022-11-24 12:28:08 +0000

    Implemented cooldown for the GunBehaviour

--- Assets/Scripts/Weapons/GunBehaviour.cs
+++ Assets/Scripts/Weapons/GunBehaviour.cs
@@ -6,11 +6,18 @@ public class GunBehaviour : MonoBehaviour
     public GameObject bullet;
     public Transform shotOrigin;
     public float muzzleVelocity = 100;
+    public float secondsOfCooldown = 1;
+    
+    private float shotTime = 0;
 
     public void Shoot(ActivateEventArgs args)
     {
-        Instantiate(bullet, shotOrigin.position, Quaternion.Euler(shotOrigin.position));
-        bullet.GetComponent<Rigidbody>().AddForce(new Vector3(0, 0, muzzleVelocity), ForceMode.Impulse);
+        if (Time.time - shotTime >= secondsOfCooldown)
+        {
+            GameObject bulletShot = Instantiate(bullet, shotOrigin.position, Quaternion.Euler(shotOrigin.position));
+            bulletShot.GetComponent<Rigidbody>().AddForce(shotOrigin.forward * muzzleVelocity, ForceMode.Impulse);
+            shotTime = Time.time;
+        }
     }
 
 }
